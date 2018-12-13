e754acadaf057fb208cb70b8722241b593b1aae4
Maharoz <mahruj66@gmail.com>
2018-12-13 21:51:05 +0600

    typeof implemented

--- index.js
+++ index.js
@@ -3,3 +3,12 @@ let age=30; //Number literal
 let isApproved = false; //Boolean literal
 let firstName; //by default value is undefined
 let lastName = null; // value is initially null
+
+/*
+
+by writting 
+    typeof isApproved 
+    -->"boolean"
+
+*/
+
