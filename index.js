149134b5ce8b4bab6470b4bdc5e91e5c61769a01
Maharoz <mahruj66@gmail.com>
2018-12-13 23:38:02 +0600

    Equality operators implemented correction

--- index.js
+++ index.js
@@ -5,6 +5,6 @@ console.log('1' === 1); //false
 
 //lose equality
 
-console.log(1==1);
-console.log('1'==1);
-console.log(true ==1);
+console.log(1==1); //true
+console.log('1'==1); //true
+console.log(true ==1); //true
