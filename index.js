7d51a1982df0145397672ccd6e26f13c1097d18f
Maharoz <mahruj66@gmail.com>
2018-12-13 23:18:30 +0600

    function implemented-2

--- index.js
+++ index.js
@@ -1,10 +1,15 @@
-let number = 1;
-
 
 //creating function
-function greet(name){
-    console.log('hello ' + name);
+function greet(name,lastName){
+    console.log('hello ' + name + ' ' + lastName);
 }
+
+//calculating a value
+function square(number){
+    return number*number;
+}
+let number = square(2);
+console.log(number);
 
 
 greet('maharoz'); //calling function
