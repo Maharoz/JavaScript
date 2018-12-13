ccd84b3c1ee46c9abfd262298ff95bc402e63436
Maharoz <mahruj66@gmail.com>
2018-12-13 23:51:16 +0600

    Logical operators implemented correction

--- index.js
+++ index.js
@@ -1,8 +1,25 @@
-// If a customer has more than 100 points,
-//they are 'gold' customer, otherwise,
-//they are a 'silver' customer.
+// Logical AND (&&)
+//returns true if both operands are true
+let highIncome = true;
+let goodCreditScore = true;
+let eligibleForLoan = highIncome && goodCreditScore;
 
-let points = 110;
-let type = points>100 ? 'gold' : 'silver';
+console.log(eligibleForLoan);
 
-console.log(type);
+// Logical OR (||)
+//returns true if both operands are true
+
+
+let highIncome = true;
+let goodCreditScore = false;
+let eligibleForLoan = highIncome || goodCreditScore;
+
+console.log(eligibleForLoan);
+
+
+//Logical NOT (!)
+
+let applicationRefused = !eligibleForLoan;
+
+console.log(eligibleForLoan);
+
