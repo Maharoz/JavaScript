42a5bcf502274346d1cc0144e8bf85f94ad72bc2
Maharoz <mahruj66@gmail.com>
2018-12-14 11:02:22 +0600

    fizz buzz implemented

--- index.js
+++ index.js
@@ -1,7 +1,26 @@
-//Landscape or potrait
+//Divisible by 3 => Fizz
+//Divisible by 5 => Buzz
+//Divisible by both 3 and 5 =>FizzBuzz
+//Not divisible by 3 or 5 => input
+//Not a number =>'Not a number
 
-function isLandscape(width,height){
-    return (width > height) ;
+const output = fizzBuzz(16);
+console.log(output);
+
+function fizzBuzz(number){
+    if (typeof (number) !== 'number')
+    return 'not a number'
+
+    else if ( number % 3 == 0 && number % 5 == 0)
+        return 'FizzBuzz'
+
+    if( number % 3 === 0)
+        return 'fizz'
+    else if (number % 5 === 0 )
+        return 'buzz'
+
+       return number
+
+      
+
 }
-
-console.log(isLandscape(800,600));
