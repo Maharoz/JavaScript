e8d2772d8706c010564ef751c12f6665de736543
Maharoz <mahruj66@gmail.com>
2018-12-14 09:16:43 +0600

    swapping two variable implemented

--- index.js
+++ index.js
@@ -1,6 +1,15 @@
-//1= 00000001
-//2= 000000010
-//3= 000000011
+//swapping two variable
 
-console.log(1 | 2); //Bit wise OR
-console.log(1 & 2); //Bit wise And
+let a = 'red';
+let b ='blue';
+
+
+let c= a;
+
+a= b;
+
+b= c;
+
+console.log(a);
+console.log(b);
+
