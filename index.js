0e187f6737db6956fb4de31022ab114731117342
Maharoz <mahruj66@gmail.com>
2018-12-14 12:49:47 +0600

    factory function implemented

--- index.js
+++ index.js
@@ -1,16 +1,20 @@
-//Creating object
 
-const circle = {
-    radius: 1,
-    location : {
-        x : 1 , 
-        y : 1
-    },
-    isVisible : true,
-    draw: function(){
-        console.log('draw');
-    }
-};
+//Factory Function
 
+function createCircle(radius){
 
-circle.draw(); // method calling
+    return {
+        radius,
+
+        draw(){
+            console.log('draw');
+        },
+
+    };
+}
+
+const circle1 = createCircle(1);
+console.log(circle1);
+
+const circle2 = createCircle(2);
+console.log(circle2);
