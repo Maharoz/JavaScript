df5a5781c08500cfab2c72da42653febc97baa60
Maharoz <mahruj66@gmail.com>
2018-12-14 08:08:49 +0600

    Logical operators implemented with non boolean

--- index.js
+++ index.js
@@ -1,25 +1,6 @@
-// Logical AND (&&)
-//returns true if both operands are true
-let highIncome = true;
-let goodCreditScore = true;
-let eligibleForLoan = highIncome && goodCreditScore;
+let userColor ='red';
+let defaultColor = 'blue';
+let currentColor = userColor || defaultColor; 
 
-console.log(eligibleForLoan);
-
-// Logical OR (||)
-//returns true if both operands are true
-
-
-let highIncome = true;
-let goodCreditScore = false;
-let eligibleForLoan = highIncome || goodCreditScore;
-
-console.log(eligibleForLoan);
-
-
-//Logical NOT (!)
-
-let applicationRefused = !eligibleForLoan;
-
-console.log(eligibleForLoan);
+console.log(currentColor);//red;
 
