0cf85b162ee2dc66b0ef3f58843d91c0f81ee5ab
Maharoz <mahruj66@gmail.com>
2018-12-14 13:00:53 +0600

    Dynamic nature of object implemented

--- index.js
+++ index.js
@@ -1,30 +1,13 @@
+const circle = {
+    radius : 1
+};
 
-//Factory Function
+circle.color = ' yellow';
+circle.draw = function(){
 
-function createCircle(radius){
-
-    return {
-        radius,
-
-        draw(){
-            console.log('draw');
-        },
-
-    };
 }
 
-const circle1 = createCircle(1);
-console.log(circle1);
+delete circle.color;
+delete circle.draw;
 
-const circle2 = createCircle(2);
-console.log(circle2);
-
-//Constructor function
-function Circle(radius){
-    this.radius = radius;
-    this.draw = function(){
-        console.log('draw');
-    }
-}
-
-const circle = new Circle(1);
+console.log(circle);
