0cd820f43c400f4eeb9cd4e65de8a3b6f24a4a99
Maharoz <mahruj66@gmail.com>
2022-10-14 18:47:14 +0600

    Types of data can be stored in an array

--- Array/app.js
+++ Array/app.js
@@ -18,3 +18,19 @@ console.log(listItems);
 
 const moreNumbers = Array.from("HI");
 console.log(moreNumbers);
+
+const hobbies = ["Cookings", "Sports"];
+const personalData = [30, "Max", { moreDetail: [] }];
+
+const analyticData = [
+  [1, 1.16],
+  [-5.4, 2.1],
+];
+
+for (const data of analyticData) {
+  for (const datapoints of data) {
+    console.log(datapoints);
+  }
+}
+
+console.log(personalData[1]);
