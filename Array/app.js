7e62870867cd8cb89c6092de3033a964e6f7ca96
Maharoz <mahruj66@gmail.com>
2022-10-14 18:41:41 +0600

    Creating Arrays

---
+++ Array/app.js
@@ -0,0 +1,20 @@
+numbers = [1, 2, 3];
+console.log(numbers);
+
+// const moreNumbers = new Array("HI", "Welcome");
+// console.log(moreNumbers);
+
+// const moreNumbers = new Array(5);
+// console.log(moreNumbers);
+
+// const yetMoreNumber = Array.of(1, 2, 3);
+// console.log(yetMoreNumber);
+
+// const moreNumbers = Array.from([1, 2, 3]);
+// console.log(moreNumbers);
+
+const listItems = document.querySelectorAll("li");
+console.log(listItems);
+
+const moreNumbers = Array.from("HI");
+console.log(moreNumbers);
--- index.js
+++
@@ -1,7 +0,0 @@
-const numbers = [1,2,3,4];
-
-console.log(numbers.indexOf('1')); //-1
-console.log(numbers.lastIndexOf(1));//3
-
-console.log(numbers.indexOf(1) !== -1); //true
-console.log(numbers.includes(1)); //true
