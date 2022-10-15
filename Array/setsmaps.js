e601a625a8a5fb13222f875ce9e45323ff11f575
Maharoz <mahruj66@gmail.com>
2022-10-15 15:25:13 +0600

    Working with Maps

--- Array/setsmaps.js
+++ Array/setsmaps.js
@@ -1,7 +1,28 @@
-const ids = new Set([1, 2, 3]);
-ids.add(2);
-console.log(ids.has(1));
+// const ids = new Set([1, 2, 3]);
+// ids.add(2);
+// console.log(ids.has(1));
 
-for (const entry of ids.entries()) {
-  console.log(entry[0]);
+// for (const entry of ids.entries()) {
+//   console.log(entry[0]);
+// }
+
+const person1 = { name: "Max" };
+const person2 = { name: "Manuel" };
+
+const personData = new Map([[person1, [{ date: "yesterday", price: 10 }]]]);
+
+personData.set(person2, [{ date: "two weeks ago", price: 100 }]);
+console.log(personData);
+console.log(personData.get(person1));
+
+for (const entry of personData.entries()) {
+  console.log(entry);
+}
+
+for (const key of personData.keys()) {
+  console.log(key);
+}
+
+for (const value of personData.values()) {
+  console.log(value);
 }
