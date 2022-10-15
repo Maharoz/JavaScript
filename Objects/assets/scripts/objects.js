6fd6994cd027604991eb0ed1a8100c1248ced6da
Maharoz <mahruj66@gmail.com>
2022-10-15 23:44:41 +0600

    shorthand property syntax

--- Objects/assets/scripts/objects.js
+++ Objects/assets/scripts/objects.js
@@ -1,14 +1,28 @@
-person = {
-  name: "Max",
-  age: 30,
-  hobbies: ["Sports", "Cooking"],
-  greet: function () {
-    alert("Hi There!");
-  },
+const addMovieBtn = document.getElementById("add-movie-btn");
+const searchBtn = document.getElementById("search-btn");
+
+const movies = [];
+
+const addMovieHandler = () => {
+  const title = document.getElementById("title").value;
+  const extraName = document.getElementById("extra-name").value;
+  const extraValue = document.getElementById("extra-value").value;
+
+  if (
+    title.trim() === "" ||
+    extraName.trim() === "" ||
+    extraValue.trim() === ""
+  ) {
+    return;
+  }
+
+  const newMovie = {
+    info: { title, [extraName]: extraName },
+    id: Math.random(),
+  };
+
+  movies.push(newMovie);
+  console.log(movies);
 };
 
-person.age = 31;
-delete person.age;
-person.isAdmin = true;
-
-console.log(person);
+addMovieBtn.addEventListener("click", addMovieHandler);
