35b32a826a453575ab347f1802faae7fe381aec3
Maharoz <mahruj66@gmail.com>
2022-10-15 12:39:29 +0600

    split() and join()

--- Array/app.js
+++ Array/app.js
@@ -128,3 +128,14 @@ const sum = prices.reduce((prevValue, curValue) => {
 }, 0);
 
 console.log(sum);
+
+const data = "new york;10.99;2000";
+
+const transformedData = data.split(";");
+
+transformedData[1] = +transformedData[1];
+console.log(transformedData);
+
+const nameFragments = ["Max", "Schwarz"];
+const name = nameFragments.join();
+console.log(name);
